34d7cb0589b9df63c348cbbb8f8c791bebce4790
dany <dany@nicoledeMacBook-Pro.local>
2015-11-28 21:22:53 +0800

    fix settings

--- ILSpyMac/AssemblyInfo.cs
+++ ILSpyMac/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("dany")]
+[assembly: AssemblyCopyright("aerror")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
