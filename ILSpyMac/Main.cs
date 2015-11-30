2824de094956fc635b135dca1e849e1aa0bdb45d
aerror2 <aerror@outlook.com>
2015-11-30 21:45:17 +0800

    fix il support

--- ILSpyMac/Main.cs
+++ ILSpyMac/Main.cs
@@ -16,6 +16,8 @@ namespace Decomplier
 	class MainClass
 	{
 
+		private const string LAN_TYPE_CSHARP = "csharp";
+		private const string LAN_TYPE_IL	 = "il";
 		private static void showUsage()
 		{
 			Console.WriteLine (" Usage: ILSpyMac [options] directory/to/all/your/dll ");
@@ -24,7 +26,7 @@ namespace Decomplier
 			Console.WriteLine (" options:");
 			Console.WriteLine ("       -n  Solution Name");
 			Console.WriteLine ("       -l  References dll path which dll will be loaded but not decompile , they use as References.");
-
+			Console.WriteLine ("       -t  Output language type, accept il or csharp, default is csharp.");
 			Console.WriteLine (" Example:");
 			Console.WriteLine (" ILSpyMac -n Example -l /directory/to/Rerences/dll /directory/to/all/your/dll");
 
@@ -33,20 +35,20 @@ namespace Decomplier
 
 		public static void Main (string[] args)
 		{
+
 			string appPath = null;
 			string slnName = null;
 			string libPath = null;
 			string expOpt = null;
-
+			string outLanguageType = LAN_TYPE_CSHARP;
 			//parsing args
 			foreach (string x in args) {
 
 				if (expOpt == null) {
 					switch (x) {
 					case "-n":
-						expOpt = x;
-						continue;
 					case "-l":
+					case "-t":
 						expOpt = x;
 						continue;
 					
@@ -72,6 +74,16 @@ namespace Decomplier
 					case "-l":
 						libPath = x;
 						break;
+					case "-t":
+						if(x!=LAN_TYPE_CSHARP && x!=LAN_TYPE_IL)
+						{
+							Console.WriteLine (" Unexpected Output language type: " + x);
+							showUsage();
+							return ;
+						}
+						outLanguageType = x;
+
+						break;
 					default:
 						showUsage ();
 						return;
@@ -91,14 +103,12 @@ namespace Decomplier
 				return;
 			}
 
-			if (slnName == null) {
+			if (slnName == null && outLanguageType==LAN_TYPE_CSHARP) {
 
 				Console.WriteLine ("Solution Name missing");
 				showUsage ();
 				return;
 			}
-
-
 
 
 			Console.WriteLine ("Decompiling all dll in  " + appPath);
@@ -135,19 +145,24 @@ namespace Decomplier
 			decompilationOptions.FullDecompilation = true;
 			decompilationOptions.assenmlyList = asmlist;
 			decompilationOptions.DecompilerSettings = new DecompilerSettings ();
+			if(outLanguageType==LAN_TYPE_CSHARP)
+			{
+				foreach (LoadedAssembly asm in ls) {
+					if (asm.IsAutoLoaded)
+						continue;
 
-			foreach (LoadedAssembly asm in ls) {
-				if (asm.IsAutoLoaded)
-					continue;
 
-				string projectPath = appPath + "/"+ asm.ShortName;
-				if(!Directory.Exists(projectPath))
-				{
-					Directory.CreateDirectory (projectPath);
+
+
+					string projectPath = appPath + "/"+ asm.ShortName;
+					if(!Directory.Exists(projectPath))
+					{
+						Directory.CreateDirectory (projectPath);
+					}
+					string projectFileName = projectPath + "/" + asm.ShortName + ".csproj";
+					asm.ProjectGuid = Guid.NewGuid();
+					asm.ProjectFileName = projectFileName;
 				}
-				string projectFileName = projectPath + "/" + asm.ShortName + ".csproj";
-				asm.ProjectGuid = Guid.NewGuid();
-				asm.ProjectFileName = projectFileName;
 			}
 
 
@@ -158,52 +173,63 @@ namespace Decomplier
 				if (asm.IsAutoLoaded)
 					continue;
 
-				var csharpLanguage = new CSharpLanguage ();
-				var textOutput = new PlainTextOutput ();
-				decompilationOptions.SaveAsProjectDirectory =  appPath + "/"+ asm.ShortName;
+				if(outLanguageType==LAN_TYPE_CSHARP)
+				{
+					var csharpLanguage = new CSharpLanguage ();
+					var textOutput = new PlainTextOutput ();
+					decompilationOptions.SaveAsProjectDirectory =  appPath + "/"+ asm.ShortName;
 
-				csharpLanguage.DecompileAssembly (asm, textOutput, decompilationOptions);
-				File.WriteAllText (asm.ProjectFileName, textOutput.ToString ());
+					csharpLanguage.DecompileAssembly (asm, textOutput, decompilationOptions);
+					File.WriteAllText (asm.ProjectFileName, textOutput.ToString ());
 
-				
-				Guid createdProjGuid = asm.ProjectGuid;
-				
-				projSln.Append("   Project(\"{");
-				projSln.Append (slnProjGuid.ToString());
-				projSln.Append ("}\") = \"");
-				projSln.Append (asm.ShortName);
-				projSln.Append ("\", \"");
-				projSln.Append (asm.ShortName+"/"+ asm.ShortName + ".csproj");
-				projSln.Append ("\", \"{");
-				projSln.Append (createdProjGuid.ToString());
-				projSln.Append ("}\"\n");
-				projSln.Append("EndProject\n");
+					
+					Guid createdProjGuid = asm.ProjectGuid;
+					
+					projSln.Append("   Project(\"{");
+					projSln.Append (slnProjGuid.ToString());
+					projSln.Append ("}\") = \"");
+					projSln.Append (asm.ShortName);
+					projSln.Append ("\", \"");
+					projSln.Append (asm.ShortName+"/"+ asm.ShortName + ".csproj");
+					projSln.Append ("\", \"{");
+					projSln.Append (createdProjGuid.ToString());
+					projSln.Append ("}\"\n");
+					projSln.Append("EndProject\n");
 
-				globSec.Append ("   {"+createdProjGuid.ToString()+"}.Debug|Any CPU.ActiveCfg = Debug|Any CPU\n");
-				globSec.Append ("   {"+createdProjGuid.ToString()+"}.Debug|Any CPU.Build.0 = Debug|Any CPU\n");
-				globSec.Append ("   {"+createdProjGuid.ToString()+"}.Release|Any CPU.ActiveCfg = Release|Any CPU\n");
-				globSec.Append ("   {"+createdProjGuid.ToString()+"}.Release|Any CPU.Build.0 = Release|Any CPU\n");
+					globSec.Append ("   {"+createdProjGuid.ToString()+"}.Debug|Any CPU.ActiveCfg = Debug|Any CPU\n");
+					globSec.Append ("   {"+createdProjGuid.ToString()+"}.Debug|Any CPU.Build.0 = Debug|Any CPU\n");
+					globSec.Append ("   {"+createdProjGuid.ToString()+"}.Release|Any CPU.ActiveCfg = Release|Any CPU\n");
+					globSec.Append ("   {"+createdProjGuid.ToString()+"}.Release|Any CPU.Build.0 = Release|Any CPU\n");
+				}
+				else
+				{
+					var ilLanguage = new ILLanguage(true);
+					var textOutput = new PlainTextOutput ();
+					ilLanguage.DecompileAssembly (asm, textOutput, decompilationOptions);
+					string ilFileName = appPath + "/"+ asm.ShortName+".il";
+					File.WriteAllText(ilFileName,textOutput.ToString());
+				}
 
 			}
 
-
-			projSln.Append("Global\n");
+			if (outLanguageType == LAN_TYPE_CSHARP) {
+				projSln.Append ("Global\n");
 				projSln.Append ("GlobalSection(SolutionConfigurationPlatforms) = preSolution\n");
 				projSln.Append ("\t\t\t\tDebug|Any CPU = Debug|Any CPU\n");
 				projSln.Append ("\t\t\t\tRelease|Any CPU = Release|Any CPU\n");
 				projSln.Append ("EndGlobalSection\n");
 
-				projSln.Append("GlobalSection(ProjectConfigurationPlatforms) = postSolution\n");
-				projSln.Append(globSec.ToString());
-				projSln.Append("EndGlobalSection\n");
+				projSln.Append ("GlobalSection(ProjectConfigurationPlatforms) = postSolution\n");
+				projSln.Append (globSec.ToString ());
+				projSln.Append ("EndGlobalSection\n");
 
-				projSln.Append("GlobalSection(MonoDevelopProperties) = preSolution\n");
-			    projSln.Append("\nEndGlobalSection\n");
-			projSln.Append("EndGlobal\n\t\t");
+				projSln.Append ("GlobalSection(MonoDevelopProperties) = preSolution\n");
+				projSln.Append ("\nEndGlobalSection\n");
+				projSln.Append ("EndGlobal\n\t\t");
 
-			string slnFileName = appPath + "/" + slnName + ".sln";
-			File.WriteAllText (slnFileName, projSln.ToString ());
-
+				string slnFileName = appPath + "/" + slnName + ".sln";
+				File.WriteAllText (slnFileName, projSln.ToString ());
+			}
 		
 
 		}
